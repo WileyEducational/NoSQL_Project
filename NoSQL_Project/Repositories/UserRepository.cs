c0dd2734cf1d3362f0ece564e57507aa72e1fa53
NietDeKaasboer <ditis@kaas.nl>
2025-09-15 00:01:04 +0200

    initial

---
+++ NoSQL_Project/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using NoSQL_Project.Models;
+using NoSQL_Project.Repositories.Interfaces;
+
+namespace NoSQL_Project.Repositories
+{
+
+    public class UserRepository : IUserRepository
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserRepository(IMongoDatabase db)
+        {
+            _users = db.GetCollection<User>("users");
+        }
+
+        public List<User> GetAll()
+        {
+            return _users.Find(_ => true).ToList();
+        }
+
+        public void Add(User user)
+        {
+            _users.InsertOne(user);
+        }
+    }
+}
